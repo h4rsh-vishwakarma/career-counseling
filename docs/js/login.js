ad83f98ebf5aa284fc8518c95aae0d76e62340a0
Harsh Vishwakarma <144579460+Harsh2292003@users.noreply.github.com>
2025-04-16 19:38:21 +0530

    Update login.js

--- docs/js/login.js
+++ docs/js/login.js
@@ -3,7 +3,7 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://career-counseling.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
